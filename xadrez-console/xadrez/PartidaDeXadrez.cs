c2dc841b7957dbe9270d2ce9628f6725f2ba4bc4
Felipe Rondello <ferondellodev@gmail.com>
2024-08-18 22:00:36 -0300

    jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -41,6 +41,22 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Voce nao pode se colocar em xeque! ");
             }
+            Peca p = tab.peca(destino);
+
+            //#jogadaespecial promocao
+
+            if(p is Peao)
+            {
+                if((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -53,7 +69,12 @@ namespace xadrez
             {
                 terminada = true;
             }
-            Peca p = tab.peca(destino);
+            else
+            {
+                turno++;
+                mudaJogador();
+            }
+
 
 
             //#jogadaespecial en passant
@@ -66,9 +87,6 @@ namespace xadrez
                 vulneraveEnPassant = null;
             }
 
-
-            turno++;
-            mudaJogador();
         }
         public Peca executaMovimento(Posicao origem, Posicao destino)
         {
